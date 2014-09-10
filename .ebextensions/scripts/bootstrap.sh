1b98c10a66d0fe269fa37cba764277020865d004
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-10 16:32:23 +1000

    No specific eb configuration required

--- .ebextensions/scripts/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# determine the script path
-# ref: http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd -P`
-
-# pop path
-popd > /dev/null
