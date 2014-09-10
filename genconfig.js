7b11773e272f054aa1f14407ceed979042b453b2
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-10 14:17:01 +1000

    Modified to include template config file generation in npm start

---
+++ genconfig.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var path = require('path');
+var dot = require('dot');
+var template = dot.template(fs.readFileSync(__dirname + '/templates/config.yaml', 'utf8'));
+var configFile = path.resolve(__dirname, 'config.yaml');
+
+// initialise template data
+var data = {};
+
+fs.exists(configFile, function(exists) {
+  if (exists) {
+    return process.exit(0);
+  }
+
+  fs.writeFile(configFile, template(data), 'utf8', function(err) {
+    process.exit(err ? 1 : 0);
+  });
+});
+
