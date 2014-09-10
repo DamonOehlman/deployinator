f34618d812c8a514bf40b35b5992bfd8c32b1f93
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-10 14:21:51 +1000

    Do not strip whitespace

--- genconfig.js
+++ genconfig.js
@@ -1,7 +1,9 @@
 var fs = require('fs');
 var path = require('path');
 var dot = require('dot');
-var template = dot.template(fs.readFileSync(__dirname + '/templates/config.yaml', 'utf8'));
+var template = dot.template(fs.readFileSync(__dirname + '/templates/config.yaml', 'utf8'), {
+  strip: false
+});
 var configFile = path.resolve(__dirname, 'config.yaml');
 
 // initialise template data
